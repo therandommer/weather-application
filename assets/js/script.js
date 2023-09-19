6f63990f752c125f231f819f30e8d86f4a1b451b
Isabella <bdc190799@gmail.com>
2023-09-19 23:26:53 +0100

    Fixed class logic
    Added extra classes to streamline the logic and make the code adhere to DRY philosophy

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,50 +9,54 @@ let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 let searchHistoryObject = $("#history");
 
-function getCoords(search)
-{
-    let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=4&appid=${weatherAPIKey}`; //creating the API search 
-    fetch(queryURL).then(function(data)
+function renderHistory() {
+
+    for (let i = 0; i < searchHistory.length; i++) //generating a search history based off prior results
     {
+        let newButton = $("<button>"); //new button created in jquery
+        newButton.attr("type", "button"); //assigning a button type to the new button
+        newButton.addClass("history-button button-history"); //adding the new classes to the button
+        newButton.text(searchHistory[i]); //assigning the text of the search to the new button
+        newButton.attr("data-search", searchHistory[i]); //assigning the new data to each button
+        searchHistoryObject.append(newButton); //adds the button as a child of the searchStorage object
+    }
+}
+
+function appendSearchHistory(search) {
+    if (searchHistory.indexOf(search) !== -1) //check if the search exists already
+    {
+        return
+    }
+
+    searchHistory.push(search);
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+    searchHistoryObject.html("");
+}
+
+function getCoords(search) {
+    let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=4&appid=${weatherAPIKey}`; //creating the API search 
+    fetch(queryURL).then(function (data) {
         console.log(queryURL);
         return data.json(); //converting the data to a json object
-    }).then(function(response) //calling a function based on the response of the URL generated above
+    }).then(function (response) //calling a function based on the response of the URL generated above
     {
         console.log(response);
-        if(!response[0])
-        {
+        if (!response[0]) {
             alert("Location is not found"); //debugging null data
         }
-        else
-        {
-            if(searchHistory.indexOf(search) !== -1) //check if the search exists already
-            {
-                return
-            }
-
-            searchHistory.push(search);
-            localStorage.setItem("search-history", JSON.stringify(searchHistory));
-            searchHistoryObject.html("");
-            for (let i = 0; i < searchHistory.length; i++) //generating a search history based off prior results
-            {
-                let newButton = $("<button>"); //new button created in jquery
-                newButton.attr("type", "button"); //assigning a button type to the new button
-                newButton.addClass("history-button button-history"); //adding the new classes to the button
-                newButton.text(searchHistory[i]); //assigning the text of the search to the new button
-                newButton.attr("data-search", searchHistory[i]); //assigning the new data to each button
-                searchHistoryObject.append(newButton);
-            }
+        else {
+            appendSearchHistory(search);
+            renderHistory();
         }
     })
 }
 
 //on submission of the form do the following logic
-function submitForm(event)
-{
+function submitForm(event) {
     console.log("Reached here");
     event.preventDefault();
     let currentSearch = searchInput.val().trim();//removes unnecessary spaces from the search form
-    
+
     getCoords(currentSearch);
 }
 
